3be8a57e3983e2c4f7af4c360647ef54e945a6b6
JoJo <89042174+JoJoDevAdventure@users.noreply.github.com>
2025-05-19 12:48:46 +0100

    theme fixer

--- src/app/context/themeContext.js
+++ src/app/context/themeContext.js
@@ -1,33 +1,14 @@
 // /src/context/themeContext.jsx
 "use client"
 
-import { createContext, useContext, useEffect, useState } from "react";
+import { createContext, useContext } from "react";
 
 const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
-  const [isDarkMode, setIsDarkMode] = useState(true); // Default to dark mode
-
-  // Optional: Sync with localStorage or system preferences
-  useEffect(() => {
-    const savedTheme = localStorage.getItem("theme");
-    if (savedTheme) {
-      setIsDarkMode(savedTheme === "dark");
-    } else {
-      // Save the default (dark mode) in localStorage
-      localStorage.setItem("theme", "dark");
-    }
-  }, []);
-
-  const toggleTheme = () => {
-    const newTheme = isDarkMode ? "light" : "dark";
-    setIsDarkMode(!isDarkMode);
-    localStorage.setItem("theme", newTheme); // Save preference
-  };
-
   return (
-    <ThemeContext.Provider value={{ isDarkMode, toggleTheme }}>
-      <div className={isDarkMode ? "dark" : ""}>{children}</div>
+    <ThemeContext.Provider value={{ isDarkMode: false, toggleTheme: () => {} }}>
+      {children}
     </ThemeContext.Provider>
   );
 };
